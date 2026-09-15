12e7dc5fdc7b1a09a26b8c918e3d260c4e40a957
inscription-essai <radical_acide@hotmail.com>
2026-09-15 19:52:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuration - À mettre à jour avec votre URL Google Apps Script
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyXsC-0hz61Gps9pe1TIcQptqb4s5vUvhmHLS2UunfzSP9hXMnft4kMu-HcZX9Iu4gjCg/exec'
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzjY-KtPv1x14U_xKJIWHs8W6Bur3q6sHRPHDUhNsLBzs-Km2dDsnfu1bi7r8bhu_kkxA/exec'
 
 // Éléments du formulaire
 const form = document.getElementById('signupForm');
